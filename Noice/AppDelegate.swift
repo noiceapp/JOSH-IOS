2734242835ff8d81546a186f0cd518c920f3bd60
slingbit <you@example.com>
2015-08-09 22:25:05 -0700

    Latest video is now playing with noice, meh, and blab count

--- Noice/AppDelegate.swift
+++ Noice/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.enableLocalDatastore()
+        Parse.setApplicationId("Z7x4BZAtSYTYY84NOffRuJqz9RX72sl0uGwJrhde", clientKey: "Can2vxkfrxHOSHG8RaTutUi77mLcHMtVO4RiemBK")
+        PFAnalytics.trackAppOpenedWithLaunchOptionsInBackground(launchOptions, block: nil)
         return true
     }
 
---
+++ Noice/MainViewController.swift
@@ -0,0 +1,150 @@
+//
+//  ViewController.swift
+//  Noice
+//
+//  Created by Joshua Lee on 7/14/15.
+//  Copyright (c) 2015 Joshua Lee. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import Parse
+
+class MainViewController: UIViewController {
+    @IBOutlet weak var videoPlayer: UIWebView!
+    @IBOutlet weak var videoTitle: UILabel!
+    @IBOutlet weak var videoDescription: UITextView!
+    @IBOutlet weak var hahaButton: UIButton!
+    @IBOutlet weak var mehButton: UIButton!
+    @IBOutlet weak var blabButton: UIButton!
+    @IBOutlet weak var numberOfNoice: UILabel!
+    @IBOutlet weak var numberOfMeh: UILabel!
+    @IBOutlet weak var numberOfBlab: UILabel!
+
+    @IBAction func hahaButtonDidTouch(sender: UIButton!) {
+        sender.selected = !sender.selected
+        self.blabButton.selected = sender.selected
+    }
+    @IBAction func mehButtonDidTouch(sender: UIButton!) {
+        sender.selected = !sender.selected
+    }
+    @IBAction func blabButtonDidTouch(sender: UIButton!) {
+        sender.selected = true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        videoTitle.text = ""
+        videoDescription.text = ""
+        numberOfNoice.text = ""
+        numberOfMeh.text = ""
+        numberOfBlab.text = ""
+        
+        //haha button image
+        let hahaImage = UIImage(named: "haha")
+        let tappedHahaImage = UIImage(named: "haha-tapped") as UIImage!
+        
+        hahaButton.setImage(hahaImage, forState: UIControlState.Normal)
+        hahaButton.setImage(tappedHahaImage, forState: UIControlState.Selected)
+        hahaButton.adjustsImageWhenHighlighted = false
+        
+        //meh button image
+        let mehImage = UIImage(named: "meh")
+        let tappedMehImage = UIImage(named: "meh-tapped")
+        
+        mehButton.setImage(mehImage, forState: UIControlState.Normal)
+        mehButton.setImage(tappedMehImage, forState: UIControlState.Selected)
+        mehButton.adjustsImageWhenHighlighted = false
+        
+        //blab button image
+        let blabImage = UIImage(named: "blab")
+        let tappedBlabImage = UIImage(named: "blab-tapped")
+        
+        blabButton.setImage(blabImage, forState: UIControlState.Normal)
+        blabButton.setImage(tappedBlabImage, forState: UIControlState.Selected)
+        blabButton.adjustsImageWhenHighlighted = false
+        
+        //video webview playback
+        videoPlayer.allowsInlineMediaPlayback = true
+        videoPlayer.mediaPlaybackRequiresUserAction = false
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        var query = PFQuery(className: "Video")
+        query.limit = 10
+        query.orderByDescending("createdAt")
+        query.findObjectsInBackgroundWithBlock {(results: [AnyObject]?, error: NSError?) -> Void in
+            if error == nil {
+                if let parseObjects = results as? [PFObject] {
+                    if let videoObject = parseObjects.first {
+                        let parseObject = videoObject
+                        //get the url and start playing the video
+                        let videoUrl : String = videoObject["url"] as! String
+                        let urlComponents = NSURLComponents(URL: NSURL(string: videoUrl)!, resolvingAgainstBaseURL: false)
+                        let parameters = urlComponents?.queryItems
+                        if let queryParameters = parameters as? [NSURLQueryItem] {
+                            for parameter in queryParameters {
+                                if parameter.name == "v" {
+                                    let videoId : String! = parameter.value
+                                    self.playYoutubeVideoWithId(videoId)
+                                }
+                            }
+                        }
+                        
+                        //get the title and description
+                        self.videoTitle.text = videoObject["title"] as? String
+                        self.videoDescription.text = videoObject["description"] as! String
+                        
+                        //go fetch voting information
+                        var voteQuery = PFQuery(className: "Vote")
+                        voteQuery.whereKey("video", equalTo: videoObject)
+                        voteQuery.findObjectsInBackgroundWithBlock {(results: [AnyObject]?, error: NSError?) -> Void in
+                            if error == nil {
+                                if let parseVoteObjects = results as? [PFObject] {
+                                    var numberOfNoice = 0
+                                    var numberOfMeh = 0
+                                    for parseVoteObject in parseVoteObjects {
+                                        let voteValue : NSNumber = parseVoteObject["value"] as! NSNumber
+                                        if voteValue.integerValue == 1 {
+                                            numberOfNoice++
+                                        }
+                                        else if voteValue.integerValue == -1 {
+                                            numberOfMeh++
+                                        }
+                                    }
+                                    self.numberOfNoice.text = String("\(numberOfNoice) noice")
+                                    self.numberOfMeh.text = String("\(numberOfMeh) meh")
+                                }
+                            }
+                        }
+                        //go fetch sharing information
+                        var shareQuery = PFQuery(className: "Share")
+                        shareQuery.whereKey("video", equalTo: videoObject)
+                        shareQuery.findObjectsInBackgroundWithBlock {(results: [AnyObject]?, error: NSError?) -> Void in
+                            if error == nil {
+                                if let numberOfBlab = results {
+                                    if numberOfBlab.count > 0 {
+                                        self.numberOfBlab.text = String("\(numberOfBlab.count) blab")
+                                    }
+                                    else {
+                                        self.numberOfBlab.text = "Be the first!"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func playYoutubeVideoWithId(videoId: String) {
+        let youTubeVideoHTML = "<!DOCTYPE html><html><head><style>body{margin:0px 0px 0px 0px;}</style></head> <body> <div id=\"player\"></div> <script> var tag = document.createElement('script'); tag.src = \"http://www.youtube.com/player_api\"; var firstScriptTag = document.getElementsByTagName('script')[0]; firstScriptTag.parentNode.insertBefore(tag, firstScriptTag); var player; function onYouTubePlayerAPIReady() { player = new YT.Player('player', { width:'%0.0f', height:'%0.0f', videoId:'%@',playerVars: {playsinline:1}, events: { 'onReady': onPlayerReady, } }); } function onPlayerReady(event) { event.target.playVideo(); } </script> </body> </html>";
+        let htmlString = String(format: youTubeVideoHTML, arguments: [videoPlayer.bounds.width, videoPlayer.bounds.height, videoId])
+        videoPlayer.loadHTMLString(htmlString, baseURL: NSBundle.mainBundle().resourceURL)
+    }
+}
+
