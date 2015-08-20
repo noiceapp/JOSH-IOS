0fa86fd0359465b9ae4896a8276d7bcffd75277d
slingbit <you@example.com>
2015-08-20 00:43:49 -0700

    ui fix

--- Noice/MainViewController.swift
+++ Noice/MainViewController.swift
@@ -244,11 +244,6 @@ class MainViewController: UIViewController, UIWebViewDelegate {
             videoPlayer.allowsInlineMediaPlayback = true
         }
     }
-
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        statusBarView.frame = CGRectMake(0,0,view.bounds.size.width,20)
-    }
     
     func playYoutubeVideoWithId(videoId: String) {
         let youTubeVideoHTML = "<!DOCTYPE html><html><head><style>body{margin:0px 0px 0px 0px;}</style></head> <body> <div id=\"player\"></div> <script> var tag = document.createElement('script'); tag.src = \"http://www.youtube.com/player_api\"; var firstScriptTag = document.getElementsByTagName('script')[0]; firstScriptTag.parentNode.insertBefore(tag, firstScriptTag); var player; function onYouTubePlayerAPIReady() { player = new YT.Player('player', { width:'%0.0f', height:'%0.0f', videoId:'%@',playerVars: {controls:0, playsinline:1}, events: { 'onReady': onPlayerReady, } }); } function onPlayerReady(event) { event.target.playVideo(); } </script> </body> </html>";
